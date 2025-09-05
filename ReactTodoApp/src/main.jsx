946d4cd6fd56eff5dc2a144a15490c5e81d54d73
Phong <thieu.phong-le95@gmail.com>
2025-09-05 13:02:34 +0200

    Simple todo list on react

--- ReactTodoApp/src/main.jsx
+++ ReactTodoApp/src/main.jsx
@@ -1,4 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById("root")).render(<h1>Hello React</h1>);
+function TaskApplication() {
+  const [tasks, setTasks] = useState([
+    { description: "Create frontend application", completed: false },
+    { description: "something", completed: false },
+  ]);
+
+  const [description, setDescription] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setTasks((old) => [...old, { description }]);
+  }
+
+  return (
+    <>
+      <h1>My todo app</h1>
+      <ul>
+        {tasks.map((t) => (
+          <li>
+            <input type={"checkbox"} checked={t.completed} />
+            {t.description}
+          </li>
+        ))}
+      </ul>
+      <h2>New task</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <strong>Task description</strong>
+          <input
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div>
+          <button>Submit{description}</button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+createRoot(document.getElementById("root")).render(<TaskApplication />);
