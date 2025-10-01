1246194cb757ca1ffb249538299f1882ee091634
Phong <thieu.phong-le95@gmail.com>
2025-10-01 15:09:41 +0200

    Task item added

--- src/application.tsx
+++ src/application.tsx
@@ -1,11 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import type { TaskItem } from "../shared/taskItem.js";
 
 export function Application() {
-  const [tasks, setTasks] = useState([
-    { description: "Create project", completed: true },
-    { description: "Create React webapp", completed: false },
-    { description: "Create Hono Backend", completed: false },
-  ]);
+  const [tasks, setTasks] = useState<TaskItem[]>([]);
+
+  async function loadTasks() {
+    const res = await fetch("/api/tasks");
+    setTasks(await res.json());
+  }
+
+  useEffect(() => {
+    loadTasks();
+  }, []);
+
   return (
     <>
       <h1>My Task Manager</h1>
